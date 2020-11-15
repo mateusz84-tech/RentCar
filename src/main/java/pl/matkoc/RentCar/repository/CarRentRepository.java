09c6c9b461a2570471d4271ce5e1aba2bed05ede
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-11-15 14:55:07 +0100

    Adding method to the find rentals by id.

--- src/main/java/pl/matkoc/RentCar/repository/CarRentRepository.java
+++ src/main/java/pl/matkoc/RentCar/repository/CarRentRepository.java
@@ -3,9 +3,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.matkoc.RentCar.domain.model.CarRental;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CarRentRepository extends JpaRepository<CarRental, Long> {
 
     @Override
     List<CarRental> findAll();
+
+    @Override
+    Optional<CarRental> findById(Long aLong);
+
+    @Override
+    CarRental getOne(Long aLong);
 }
