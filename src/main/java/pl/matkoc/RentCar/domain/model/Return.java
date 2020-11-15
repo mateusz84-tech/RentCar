5dac8649b5a0a36d985624cfd2593fdca34c62f2
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-11-15 13:15:04 +0100

    initial commit

--- src/main/java/pl/matkoc/RentCar/domain/model/Return.java
+++ src/main/java/pl/matkoc/RentCar/domain/model/Return.java
@@ -3,7 +3,6 @@ package pl.matkoc.RentCar.domain.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.Parent;
 
 import javax.persistence.*;
 
