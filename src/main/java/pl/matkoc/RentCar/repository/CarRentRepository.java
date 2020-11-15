d50f3153dbfb6a3b71d6cd8a1ab86c3456773860
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-11-15 13:09:16 +0100

    Added method JPA o display all CarRents.

--- src/main/java/pl/matkoc/RentCar/repository/CarRentRepository.java
+++ src/main/java/pl/matkoc/RentCar/repository/CarRentRepository.java
@@ -1,7 +1,11 @@
 package pl.matkoc.RentCar.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.matkoc.RentCar.domain.model.CarRental;
 
+import java.util.List;
+
 public interface CarRentRepository extends JpaRepository<CarRental, Long> {
+
+    @Override
+    List<CarRental> findAll();
 }
