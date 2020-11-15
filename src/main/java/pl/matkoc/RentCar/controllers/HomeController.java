38973ce78087f2bfd3ca8efb2a9cd1b7c7719e94
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-11-15 13:07:26 +0100

    Added action to show all CarRental.

--- src/main/java/pl/matkoc/RentCar/controllers/HomeController.java
+++ src/main/java/pl/matkoc/RentCar/controllers/HomeController.java
@@ -2,15 +2,29 @@ package pl.matkoc.RentCar.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.matkoc.RentCar.domain.model.CarRental;
+import pl.matkoc.RentCar.repository.CarRentRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
+    private CarRentRepository rentRepository;
+
+    public HomeController(CarRentRepository rentRepository) {
+        this.rentRepository = rentRepository;
+    }
+
     @GetMapping
     public String showHomePage(){
         return "/mainPages/homePage";
-
+    }
+    @ModelAttribute("rent")
+    public List<CarRental> carRentalList(){
+        return rentRepository.findAll();
     }
 }
