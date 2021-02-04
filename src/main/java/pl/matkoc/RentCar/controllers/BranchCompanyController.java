9302174a611eeb8fff21007fee2302b05294ec2c
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-02-04 20:07:05 +0100

    Added to action - add newBranch - annotation Valid and new argument BindingResult, also add annotation ModelAttribute(newBrach), because Spring automatic create an add to model name class first letters low.

--- src/main/java/pl/matkoc/RentCar/controllers/BranchCompanyController.java
+++ src/main/java/pl/matkoc/RentCar/controllers/BranchCompanyController.java
@@ -2,13 +2,19 @@ package pl.matkoc.RentCar.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.matkoc.RentCar.domain.model.BranchOfCompany;
 import pl.matkoc.RentCar.repository.BranchCompanyRepository;
+
+import javax.validation.Valid;
+import javax.validation.Validator;
 
 @Controller
 @RequestMapping("/branch")
@@ -26,9 +32,16 @@ public class BranchCompanyController {
         model.addAttribute("newBranch",new BranchOfCompany());
         return "rent/addNewBrach";
     }
+
+
     @PostMapping("/add")
-    public String processAddNewBranchCompany(BranchOfCompany branchOfCompany){
+    public String processAddNewBranchCompany(@ModelAttribute("newBranch")
+                                                 @Valid BranchOfCompany branchOfCompany,
+                                             BindingResult result){
         logger.info("Przed zapisem: {}",getClass());
+        if(result.hasErrors()){
+            return "rent/addNewBrach";
+        }
         branchCompanyRepository.save(branchOfCompany);
         logger.info("Po zapisie: {}",getClass());
         return "redirect:/";
