9b7fcea09e7074685567339eda26da7e36f7f031
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-02-04 19:04:16 +0100

    I have placed annotations over the fields.

--- src/main/java/pl/matkoc/RentCar/domain/model/BranchOfCompany.java
+++ src/main/java/pl/matkoc/RentCar/domain/model/BranchOfCompany.java
@@ -4,18 +4,41 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Range;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "branch_company")
 @Getter @Setter @EqualsAndHashCode @ToString
 public class BranchOfCompany {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotBlank
+    @NotEmpty
+    @Size(min=3, max=12)
     private String name;
+
+    @NotBlank
+    @NotEmpty
+    @Size(min=3, max=12)
     private String street;
+
+    @NotBlank
+    @NotEmpty
+    @Size(min=3, max=12)
     private String city;
+
+    @NotBlank
+    @NotEmpty
+    @Size(min=3, max=12)
     private String zipCode;
 
 //    @OneToMany(mappedBy = "branchOfCompany")
