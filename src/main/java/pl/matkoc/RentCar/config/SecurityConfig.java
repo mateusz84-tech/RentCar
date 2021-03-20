263a64822ea0af09b8841760cec472b146fc6940
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-20 17:27:49 +0100

    Add bean to password encryption.

--- src/main/java/pl/matkoc/RentCar/config/SecurityConfig.java
+++ src/main/java/pl/matkoc/RentCar/config/SecurityConfig.java
@@ -1,13 +1,20 @@
 package pl.matkoc.RentCar.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
