60a633d82fb21691f93b83566658cad616a9e362
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-20 16:08:59 +0100

    Define user name and login for OWNER and USER

--- src/main/java/pl/matkoc/RentCar/config/SecurityConfig.java
+++ src/main/java/pl/matkoc/RentCar/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package pl.matkoc.RentCar.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("owner").password("{noop}owner1").roles("OWNER")
+                .and()
+                .withUser("user").password("{noop}user1").roles("USER");
+    }
 }
