179bed2830c7344956a2225c670228f42b1aeb0f
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-21 19:19:00 +0100

    Added new beans in security.

--- src/main/java/pl/matkoc/RentCar/config/SecurityConfig.java
+++ src/main/java/pl/matkoc/RentCar/config/SecurityConfig.java
@@ -1,23 +1,25 @@
 package pl.matkoc.RentCar.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import pl.matkoc.RentCar.services.SpringDataUserDetailService;
+import pl.matkoc.RentCar.services.MyUserDetailService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SpringDataUserDetailService customUserDetailsService(){
-        return new SpringDataUserDetailService();
-    }
+    public MyUserDetailService userDetailService(){
+        return new MyUserDetailService();
+    };
+
 }
