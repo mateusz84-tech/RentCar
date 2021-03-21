cae7e3ea520444dd7c7d4346b90f8de6cf23c590
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-21 19:13:10 +0100

    New UserService using pattern design Facade.

--- src/main/java/pl/matkoc/RentCar/services/UserService.java
+++ src/main/java/pl/matkoc/RentCar/services/UserService.java
@@ -1,10 +1,44 @@
 package pl.matkoc.RentCar.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.matkoc.RentCar.domain.model.Role;
 import pl.matkoc.RentCar.domain.model.User;
+import pl.matkoc.RentCar.repository.RoleRepository;
+import pl.matkoc.RentCar.repository.UserRepository;
 
-public interface UserService {
+import java.util.Arrays;
+import java.util.HashSet;
 
-    User findByUserName(String name);
+@Service
+public class UserService {
 
-    void saveUser(User user);
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository,
+                       RoleRepository roleRepository,
+                       BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User findUserByName(String userName){
+        return userRepository.findByUserName(userName);
+    }
+
+    public User saveUser(User user){
+        if(user == null){
+            throw new NullPointerException("Użytkownik nie może być wartością null.");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setEnabled(1);
+        Role userRole = roleRepository.findByName("ADMIN");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        return userRepository.save(user);
+    }
 }
