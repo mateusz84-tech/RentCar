aac3309e313dccb647ee2ec622353280ed4a2cc6
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-23 18:57:44 +0100

    Create Login controller with access method to page.

---
+++ src/main/java/pl/matkoc/RentCar/controllers/LoginController.java
@@ -0,0 +1,34 @@
+package pl.matkoc.RentCar.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import pl.matkoc.RentCar.domain.model.User;
+import pl.matkoc.RentCar.services.UserService;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = {"/","/login"}, method = RequestMethod.GET)
+    public ModelAndView login(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login/login");
+        return modelAndView;
+    }
+
+    @GetMapping("/registration")
+    public ModelAndView registration(){
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user",user);
+        modelAndView.setViewName("registration");
+        return modelAndView;
+    }
+
+}
