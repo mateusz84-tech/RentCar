32e117110b205840f546986a89718c0174888b90
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-03-23 19:17:25 +0100

    Added method post to take user.

--- src/main/java/pl/matkoc/RentCar/controllers/LoginController.java
+++ src/main/java/pl/matkoc/RentCar/controllers/LoginController.java
@@ -2,12 +2,16 @@ package pl.matkoc.RentCar.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import pl.matkoc.RentCar.domain.model.User;
 import pl.matkoc.RentCar.services.UserService;
+
+import javax.validation.Valid;
 
 @Controller
 public class LoginController {
@@ -29,6 +33,26 @@ public class LoginController {
         modelAndView.addObject("user",user);
         modelAndView.setViewName("registration");
         return modelAndView;
+    }
+
+    @PostMapping("/registration")
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult){
+        ModelAndView modelANdView = new ModelAndView();
+        User userExist = userService.findUserByName(user.getUserName());
+        if(userExist != null){
+            bindingResult.rejectValue("userName","error.user",
+                    "Taki użytkownik już istnieje");
+        }
+        if(bindingResult.hasErrors()){
+            modelANdView.setViewName("login/registration");
+        }
+        else{
+            userService.saveUser(user);
+            modelANdView.addObject("Wiadomość", "Użytkownik został zarejestrowany");
+            modelANdView.addObject("user",new User());
+            modelANdView.setViewName("login/registration");
+        }
+        return modelANdView;
     }
 
 }
